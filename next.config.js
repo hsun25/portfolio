9679ec56e76fd949a8ba400402725fb41f4465f1
Sun, Hai <hsun389@gatech.edu>
2023-12-21 22:05:59 -0600

    fix links

--- next.config.js
+++ next.config.js
@@ -4,13 +4,19 @@ const withNextra = require('nextra')({
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 })
 
+const repo = 'portfolio'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // any configs you need
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath
 }
 
 module.exports = withNextra(nextConfig)
