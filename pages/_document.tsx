caca8fea1c4ceea4f7f7418091f705f854c231c0
Sun, Hai <hsun389@gatech.edu>
2023-12-21 21:31:50 -0600

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Hai (Holly) Sun Portfolio',
+    description: 'This is the portfolio website for Hai (Holly) Sun',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
   }
 
--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    title: 'Hai (Holly) Sun',
+    site_url: 'https://hai-sun-portfolio.com',
+    feed_url: 'https://hai-sun-portfolio.com/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
